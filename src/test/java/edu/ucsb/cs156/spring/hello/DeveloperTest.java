fd0d72d4fc9c9232f006efcaa975134c650b12dc
Ben Asperheim <basperheim@ucsb.edu>
2024-10-08 19:30:10 -0700

    added various additional test cases to get nearly complete mutation coverage

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,6 +25,28 @@ public class DeveloperTest {
         assertEquals("Ben", Developer.getName());
     }
 
+    @Test
+    public void getGithubId_returns_correct_githubId(){
+        assertEquals("basperheim1", Developer.getGithubId());
+    }
+    
+    @Test
+    public void getTeam_returns_team_with_correct_name(){
+        Team t = Developer.getTeam();
+        assertEquals("f24-05", t.getName());
+    }
+
+    @Test
+    public void getTeam_returns_team_with_correct_members() {
+        Team  t = Developer.getTeam();
+        assertTrue(t.getMembers().contains("Ishan"),"Team should contain Ishan");
+        assertTrue(t.getMembers().contains("Alex"),"Team should contain Alex");
+        assertTrue(t.getMembers().contains("Baige"),"Team should contain Baige");
+        assertTrue(t.getMembers().contains("William"),"Team should contain William");
+        assertTrue(t.getMembers().contains("Ryan"),"Team should contain Ryan");
+        assertTrue(t.getMembers().contains("Ben"),"Team should contain Ben");
+    }
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -19,6 +19,34 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test 
+    public void equals_method_returns_correct_value(){
+        
+        // Our overrided equals method should return true if the same object is passed as a parameter
+        assert(team.equals(team));
+
+        int x = 5; 
+        assert(!team.equals(x));
+
+        Team t1 = new Team("test-team");
+        Team t2 = new Team("wrong-team");
+        Team t3 = new Team("test-team");
+
+        t3.addMember("ben");
+
+        // Checks for equality of teams with varying names and members 
+        assert(t1.equals(team));
+        assert(!t2.equals(team));
+        assert(!t3.equals(team));
+
+
+    }
+
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
